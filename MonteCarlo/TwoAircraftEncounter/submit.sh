56204c7c7401ab470dd8b1e98718bc115e1850c8
zhihao <zhihao.quek@ntu.edu.sg>
2022-06-06 12:28:43 +0800

    Update to include latest shell scripts for running on NSCC.

--- MonteCarlo/TwoAircraftEncounter/submit.sh
+++ MonteCarlo/TwoAircraftEncounter/submit.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 #PBS -q normal
 #PBS -j oe
-#PBS -l select=1:ncpus=24:mem=96G
-#PBS -l walltime=00:30:00
+#PBS -l select=2:ncpus=24:mem=64G:mpiprocs=24:ompthreads=1
+#PBS -l walltime=00:06:00
 #PBS -P 12001778
 #PBS -N MonteCarloTestProgram
+#PBS -M zhihao.quek@ntu.edu.sg
+#PBS -m abe
 
 # Commands start here
 module load anaconda/3
+module load openmpi/gcc493/1.10.2
 cd ${PBS_O_WORKDIR}
-source activate mypyenv
-python Test_Run.py
+source activate testenv
+mpirun python -m mpi4py.futures Test_mpi4py.py
